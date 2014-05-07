180e542a0b0fc61fb719e33be5df557846566b4f
J. Ky Marsh <jkymarsh@gmail.com>
2014-05-07 01:23:13 -0500

    add working example of ui bootstrap modal

--- public/js/main.js
+++ public/js/main.js
@@ -1,47 +1,42 @@
-console.log('lol');
+var modalTestApp = angular.module('modalTestApp', ['ui.bootstrap']);
 
-angular.module('modalTest', ['ui.bootstrap']);
+modalTestApp.controller('UiBootstrapModalContainerCtrl', ['$scope', '$modal', '$log', function ($scope, $modal, $log) {
+    $scope.items = ['item1', 'item2', 'item3'];
 
-var ModalDemoCtrl = function ($scope, $modal, $log) {
+    $scope.open = function (size) {
+        var modalInstance = $modal.open({
+            templateUrl: '../tpl/demo-modal.html',
+            controller: 'UiBootstrapModalInstanceCtrl',
+            size: size,
+            resolve: {
+                items: function () {
+                    return $scope.items;
+                }
+            }
+        });
 
-  $scope.items = ['item1', 'item2', 'item3'];
-
-  $scope.open = function (size) {
-
-    var modalInstance = $modal.open({
-      templateUrl: 'myModalContent.html',
-      controller: ModalInstanceCtrl,
-      size: size,
-      resolve: {
-        items: function () {
-          return $scope.items;
-        }
-      }
-    });
-
-    modalInstance.result.then(function (selectedItem) {
-      $scope.selected = selectedItem;
-    }, function () {
-      $log.info('Modal dismissed at: ' + new Date());
-    });
-  };
-};
+        modalInstance.result.then(function (selectedItem) {
+            $scope.selected = selectedItem;
+        }, function () {
+            $log.info('Modal dismissed at: ' + new Date());
+        });
+    };
+}]);
 
 // Please note that $modalInstance represents a modal window (instance) dependency.
 // It is not the same as the $modal service used above.
+modalTestApp.controller('UiBootstrapModalInstanceCtrl', ['$scope', '$modalInstance', 'items', function ($scope, $modalInstance, items) {
+    $scope.items = items;
 
-var ModalInstanceCtrl = function ($scope, $modalInstance, items) {
+    $scope.selected = {
+        item: $scope.items[0]
+    };
 
-  $scope.items = items;
-  $scope.selected = {
-    item: $scope.items[0]
-  };
+    $scope.ok = function () {
+        $modalInstance.close($scope.selected.item);
+    };
 
-  $scope.ok = function () {
-    $modalInstance.close($scope.selected.item);
-  };
-
-  $scope.cancel = function () {
-    $modalInstance.dismiss('cancel');
-  };
-};
+    $scope.cancel = function () {
+        $modalInstance.dismiss('cancel');
+    };
+}]);
